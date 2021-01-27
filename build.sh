c49fec29a7e1baba31e0186c4b4d5580734405ab
Gerard Aakders <linuxwizd3m@gmail.com>
2021-01-27 14:13:41 +0100

    changed build script

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 #printf '\n[%s] WARNING: %s\n\n' "mkarchiso" "build.sh scripts are deprecated! Please use mkarchiso directly." >&2
-_buildsh_path="$(realpath -- "$0")"
-sudo mkarchiso "$@" "${_buildsh_path%/*}"
+#_buildsh_path="$(realpath -- "$0")"
+#sudo mkarchiso  "${_buildsh_path%/*}"
+sudo mkarchiso -v .
