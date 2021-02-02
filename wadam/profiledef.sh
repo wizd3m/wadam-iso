d275a60facb7398cdd014a1cb902fe7706ee0fa7
Gerard Aakders <linuxwizd3m@gmail.com>
2021-02-02 12:22:48 +0100

    profile and splash changes

--- wadam/profiledef.sh
+++ wadam/profiledef.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="wadamlinux"
+iso_label="WADAM_$(date +%Y%m)"
+iso_publisher="Wadam Linux <https://wadam.org>"
+iso_application="Wadam Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfx_image_tool_options=('-comp' 'zstd')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
